1f14744abd5f5815cf902df73af4890d1fd2476a
John Rykken <john.rykken@gmail.com>
2018-04-20 12:31:52 -0700

    Add create brew functionality.

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -13,4 +13,19 @@ class BrewsController < ApplicationController
     @brew = Brew.new
   end
 
+  def create
+    @brew = Brew.new(brew_params)
+    if @brew.save
+      flash[:notice] = "#{@brew.name} was added to the list"
+      redirect_to brews_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def brew_params
+      params.require(:brew).permit(:name, :price, :state)
+    end
+
 end
