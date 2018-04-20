6ccf6e1ef05736b91004f08b754b1e32a565af4a
John Rykken <john.rykken@gmail.com>
2018-04-20 10:14:52 -0700

    Create brews/reviews migrations. Run db:migrate/test:prepare.

--- db/migrate/20180420164318_create_brews.rb
+++ db/migrate/20180420164318_create_brews.rb
@@ -1,6 +1,11 @@
 class CreateBrews < ActiveRecord::Migration[5.2]
   def change
     create_table :brews do |t|
+      t.column :name, :string
+      t.column :price, :integer
+      t.column :state, :string
+
+      t.timestamps 
     end
   end
 end
--- db/migrate/20180420165649_create_reviews.rb
+++ db/migrate/20180420165649_create_reviews.rb
@@ -1,6 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
+      t.column :brew_id, :integer
+      t.column :author, :string
+      t.column :content, :string 
+      t.column :rating, :integer
+
+      t.timestamps
     end
   end
 end
