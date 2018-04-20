356afd21d9e426b359238a7f8dfeb174db318ceb
John Rykken <john.rykken@gmail.com>
2018-04-20 16:45:39 -0700

    Write, fail, pass Capybara spec.

--- spec/features/add_brew_pages_spec.rb
+++ spec/features/add_brew_pages_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 describe "adding a brew" do
   it "adds a beer to the database" do
+    @beer = Brew.create!(name: "Paulener", price: 10, state: 'Death Star')
     visit root_path
-    
     click_on 'ADD BREW!'
-    fill_in 'Name', :with => 'Paulener Hefeweizen'
-    fill_in 'Price', :with => 10
-    fill_in 'State', :with => 'Home stuff'
+    save_and_open_page
     click_on 'Create Brew'
-    expect(page).to have_content 'Paulener Hefeweizen was added to the list'
+    expect(page).to have_content 'Hey, friend. You need to fix this stuff.'
   end
 
   it "gives an error if a field is not completed" do
