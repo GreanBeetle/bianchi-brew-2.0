fbd9b27766e16b30f175758615591b7fcf40de03
John Rykken <john.rykken@gmail.com>
2018-04-20 17:24:28 -0700

    Fix db:seed issue.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@ Review.destroy_all
   @brew_id = Brew.last.id
   5.times do |i|
     author = Faker::FunnyName.three_word_name
-    content = Faker::Hipster.sentence(50)
+    content = Faker::Hipster.sentence(10)
     rating = Faker::Number.between(1, 5)
     Review.create!(brew_id: @brew_id, author: author, content: content, rating: rating)
   end
   if @brew_id == Brew.first.id
     10.times do |i|
-      Review.create!(brew_id: @brew_id, author: Faker::FunnyName.three_word_name, content: Faker::Hipster.sentence(50), rating: Faker::Number.between(1, 5))
+      Review.create!(brew_id: @brew_id, author: Faker::FunnyName.three_word_name, content: Faker::Hipster.sentence(10), rating: Faker::Number.between(1, 5))
     end
   end
 end
