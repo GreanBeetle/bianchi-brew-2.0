c9222ca9883f249dd4fffca957d27a1c90ff50fb
John Rykken <john.rykken@gmail.com>
2018-04-20 17:11:04 -0700

    Write, pass final capybara test.

---
+++ spec/features/edit_brew_pages_spec.rb
@@ -0,0 +1,11 @@
+equire 'rails_helper'
+
+describe "editing a beer" do
+  it "edits beer details" do
+    @beer = Brew.create!(name: "Joe Beer", price: 10, state: 'Death Star')
+    visit edit_brew_path(@beer)
+    fill_in 'Name', :with => 'Ernest H.'
+    click_on 'Update Brew'
+    expect(page).to have_content 'Ernest H. was edited.'
+  end
+end
